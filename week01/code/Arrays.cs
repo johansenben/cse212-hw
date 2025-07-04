28fb2b23641951877d419b1ef627b7fb19d26b9b
Ben Johansen <johansenben12@gmail.com>
2025-07-04 15:42:20 -0600

    week01 - code: dynamic arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,30 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+
+        /*
+            -the total number of elements in the array is equal to length
+            -the value at each index should be equal to number * (index + 1)
+
+            1. create a new empty array
+            2. iterate through each index of the array
+            3. set each value of the array
+                -value = number * (index + 1)
+            4. return the array
+        */
+
+        //create an empty array with a size equal to the length
+        double[] values = new double[length];
+
+        //loop with 1 iteration per value in length
+        for (int index = 0; index < length; index++)
+        {
+            //the value of values[index] is number * (index + 1)
+            values[index] = number * (index + 1);
+        }
+
+        //return the array
+        return values;
     }
 
     /// <summary>
@@ -29,5 +52,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        /*
+            1. save the slice at the end of the list, then remove it from the list
+            2. insert the slice at the beginning of the list
+        */
+
+        //save the length to access later
+        int length = data.Count; 
+
+        //save the slice from the end of the list
+        List<int> slice = data.GetRange(length - amount, amount);
+
+        //remove the slice from the end the end of the list
+        data.RemoveRange(length - amount, amount);
+
+        //add the saved slice to the beginning of the list
+        data.InsertRange(0, slice);       
     }
 }
